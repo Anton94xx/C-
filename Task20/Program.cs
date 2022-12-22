dc1db72217d97d735080c07c1fc1441dbcc1a71a
Anton <anton94xx@gmail.com>
2022-12-22 13:09:10 +0500

    задача семинара №20

---
+++ Task20/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+double d = Math.Sqrt(456);
+
+double d = 5.09986564;
+double dRes = Math.Round(d, 2, MidpointRounding.ToZero);
+System.Console.WriteLine(dRes);

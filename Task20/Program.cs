b1cefcf1f62a92722e05eced8e7779d39af4cf81
Anton <anton94xx@gmail.com>
2022-12-26 16:45:14 +0500

    Домашнее задание 21

--- Task20/Program.cs
+++ Task20/Program.cs
@@ -5,26 +5,26 @@
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
-int InsertPositon(string text)
+int InsertPosition(string text)
 {
     Console.WriteLine(text);
     int pos = Convert.ToInt32(Console.ReadLine());
     return pos;
 }
 
-int x1 = InsertPositon("Введите координату Х первой точки:");
-int y1 = InsertPositon("Введите координату Y первой точки:");
+int x1 = InsertPosition("Введите координату Х первой точки:");
+int y1 = InsertPosition("Введите координату Y первой точки:");
 
-int x2 = InsertPositon("Введите координату Х второй точки:");
-int y2 = InsertPositon("Введите координату Y второй точки:");
+int x2 = InsertPosition("Введите координату Х второй точки:");
+int y2 = InsertPosition("Введите координату Y второй точки:");
 
-double LenghtLine(int xpos1, int xpos2, int ypos1, int ypos2)
+double LengthLine(int xpos1, int xpos2, int ypos1, int ypos2)
 {
     int temp = (xpos1-xpos2)*(xpos1-xpos2) + (ypos1-ypos2)*(ypos1-ypos2);
     return Math.Sqrt(temp);
 }
 
-double res = LenghtLine(x1,x2,y1,y2);
+double res = LengthLine(x1,x2,y1,y2);
 
 double resRound = Math.Round(res,2,MidpointRounding.ToZero);
 
